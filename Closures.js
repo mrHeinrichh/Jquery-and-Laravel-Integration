56368ab8eb67b5c635cc71d37b937c99502a79e9
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-10-07 11:12:14 +0800

    Comment at custom js documentation

--- Closures.js
+++ Closures.js
@@ -1791,19 +1791,19 @@ $(document).ready(function() {
       dataType: 'json',
       success: function (data) {
           console.log(data);
-          $.each(data, function(key, value) {
-            console.log(value);
-            var id = value.id;
-            var tr = $("<tr>");
-            tr.append($("<td>").html(value.title));
-            tr.append($("<td>").html(value.id));
-            tr.append($("<td>").html(value.lname));
-            tr.append($("<td>").html(value.fname));
-            tr.append($("<td>").html(value.addressline));
-            tr.append($("<td>").html(value.phone));
-            tr.append("<td align='center'><a href="+'/customer/'+id+'/edit'+"><i class='fa fa-pencil-square-o' aria-hidden='true' style='font-size:24px' ></a></i></td>");
-            $("#cbody").append(tr);
-     });
+    //       $.each(data, function(key, value) {
+    //         console.log(value);
+    //         var id = value.id;
+    //         var tr = $("<tr>");
+    //         tr.append($("<td>").html(value.title));
+    //         tr.append($("<td>").html(value.id));
+    //         tr.append($("<td>").html(value.lname));
+    //         tr.append($("<td>").html(value.fname));
+    //         tr.append($("<td>").html(value.addressline));
+    //         tr.append($("<td>").html(value.phone));
+    //         tr.append("<td align='center'><a href="+'/customer/'+id+'/edit'+"><i class='fa fa-pencil-square-o' aria-hidden='true' style='font-size:24px' ></a></i></td>");
+    //         $("#cbody").append(tr);
+    //  });
         },
       error: function(){
         console.log('AJAX load did not work');
