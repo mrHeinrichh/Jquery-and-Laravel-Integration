1a07e650aa5569ca070e2be817aa4ee901f3affd
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-10-07 10:41:05 +0800

    Add customer model fields

--- app/Models/customer.php
+++ app/Models/customer.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class customer extends Model
 {
     use HasFactory;
+
+    protected $table = "customers";
+
+    // protected $guarded = ["id"];
+
+    protected $primaryKey = "customer_id";
+
+    protected $fillable = [
+        'title', 
+        'fname', 
+        'lname',
+        'addressline',
+        'town',
+        'zipcode',
+        'phone',
+        'creditlimit',
+        'level',
+        'user_id'
+    ];
+
+    public $timestamps = false;
 }
