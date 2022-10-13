ab3f6aa4b7a51ba6a1308aa8fd13c7231f044789
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-10-13 09:09:39 +0800

    Import external libraries

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,11 +1,8 @@
-<!doctype html>
- <html lang="en">
- <head>
- <meta charset="UTF-8">
- <title></title>
- </head>
- <body>
- @yield('body')
- @include('layouts.header')
- </body>
- </html>
+<link href="https://maxcdn.bootstrapcdn.com/.../css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/.../js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/c88097f817.js" crossorigin="anonymous"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+<script type="text/javascript">
+    var token = '{{ csrf_token() }}';
+</script>
