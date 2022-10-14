7b0096c61a2d0eb0ebf688e7c5ccd70f320ecb80
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-10-14 15:04:01 +0800

    Add item controller

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+use View;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()){
+            $items = Item::orderBy('item_id', 'DESC')->get();
+            return response()->json($items);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $items = Item::create($request->all());
+        return response()->json($items);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $items = Item::find($id);
+        return response()->json($items);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // if ($request->ajax()) {
+        $items = Item::find($id);
+        $items = $items->update($request->all());
+         return response()->json($items);
+        // } 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $items = Item::findOrFail($id);
+        $items->delete();
+        return response()->json(["success" => "item deleted successfully.",
+             "status" => 200]);
+    }
+}
