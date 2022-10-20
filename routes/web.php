14454b616ff01fbc80cc2a22ab5acff0f6db3ace
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-10-20 09:20:53 +0800

    Update web php route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('customer', 'customerController');
-
+Route::resource('customer', 'CustomerController');
+Route::resource('item', 'ItemController');
+route::get('/item-index', 'ItemController@getItem');
