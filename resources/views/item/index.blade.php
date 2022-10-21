92b4e5b582e594c4a5e1e3377cac191c00865906
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-10-21 09:58:42 +0800

    Rename button image field

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -75,8 +75,8 @@
                         <input type="text" class="form-control " id="title" name="title">
                     </div>
                     <div class="form-group">
-                        <label for="img" class="control-label">img</label>
-                        <input type="file" class="form-control" id="img" name="img" />
+                        <label for="img_path" class="control-label">img_path</label>
+                        <input type="file" class="form-control" id="img_path" name="img_path" />
                     </div>
                 </form>
             </div>
