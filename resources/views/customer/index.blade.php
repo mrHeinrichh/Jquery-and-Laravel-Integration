529b6042b2f553d2747d5db3e96a2074ed088e66
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-11-03 08:22:23 +0800

    Update index blade views

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -3,64 +3,82 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
+    <title>Customer</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
         integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"
         integrity="sha512-RdSPYh1WA6BF0RhpisYJVYkOyTzK4HwofJ3Q7ivt/jkpW6Vc8AurL1R+4AUcvn9IwEKAPm/fk7qFZW3OuiUDeg=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <script src="js/custom.js"></script>
+
 </head>
 
 <body>
-
     <div id="customers" class="container">
-        <button type="button" class="btn btn-info btn-lg" data-bs-toggle="modal" data-bs-target="#myModal">ADD <span
-                class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
-        </a>
-        <button type="button" class="btn btn-info btn-lg" id="item">Swap<span class="glyphicon glyphicon-plus"
-                aria-hidden="true"></span></button>
+        <div class="card" style="background-color: #222227;
+        box-shadow: 0 0.5rem 3rem rgba(0, 0, 0, 0.4);">
+            <div class="card-header" style="font-size: 2rem;color: white;">
+                Customer List
+            </div>
+            <div class="card-body" style="background-color: #292930;">
+                <button type="button" class="btn btn-info btn-lg" data-bs-toggle="modal" data-bs-target="#myModal">New
+                    Customer
+                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
 
-
-        <div class="table-responsive">
-            <table id="ctable" class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Customer ID</th>
-                        <th>Title</th>
-                        <th>lname</th>
-                        <th>fname</th>
-                        <th>address</th>
-                        <th>town</th>
-                        <th>zipcode</th>
-                        <th>number</th>
-                        <th>credit limit</th>
-                        <th>level</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody id="cbody">
-                </tbody>
-            </table>
+                <button type="button" class="btn btn-info btn-lg" id="item" style="float: right">Show Items<span
+                        class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
+                <div class="card" style="margin-top: 10px">
+                    <div class="table-responsive">
+                        <table id="ctable" class="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Customer ID</th>
+                                    <th>Title</th>
+                                    <th>lname</th>
+                                    <th>fname</th>
+                                    <th>address</th>
+                                    <th>town</th>
+                                    <th>zipcode</th>
+                                    <th>number</th>
+                                    <th>credit limit</th>
+                                    <th>level</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody id="cbody">
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="myModal" role="dialog" style="display:none">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -121,11 +139,14 @@
                         </div>
 
                     </form>
+
                 </div>
+
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
                     <button id="myFormSubmit" type="submit" class="btn btn-primary">Save</button>
                 </div>
+
             </div>
         </div>
     </div>
@@ -190,115 +211,15 @@
                 </div>
             </div>
         </div>
+    </div>`
+
+    <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
+        <button id="updatebtn" type="submit" class="btn btn-primary">Save</button>
     </div>
 
-    <div id="items" class="container">
-
-        <button type="button" class="btn btn-info btn-lg" data-bs-toggle="modal" data-bs-target="#itemModal">add<span
-                class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
-
-        <button type="button" class="btn btn-info btn-lg" id="customerbtn">Swap<span class="glyphicon glyphicon-plus"
-                aria-hidden="true"></span></button>
-
-        <div class="table-responsive">
-            <table id="itable" class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Item ID</th>
-                        <th>Description</th>
-                        <th>Sell price</th>
-                        <th>Cost price</th>
-                        <th>Title</th>
-                        <th>Image</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody id="ibody">
-                </tbody>
-            </table>
-        </div>
     </div>
-
-    <div class="modal fade" id="itemModal" role="dialog" style="display:none">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Create new item</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form id="iform" method="#" action="#" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <label for="desc" class="control-label">Description</label>
-                            <input type="text" class="form-control" id="desc" name="description">
-                        </div>
-                        <div class="form-group">
-                            <label for="cost" class="control-label">Cost Price</label>
-                            <input type="text" class="form-control " id="cost" name="cost_price">
-                        </div>
-                        <div class="form-group">
-                            <label for="sell" class="control-label">sell price</label>
-                            <input type="text" class="form-control " id="sell" name="sell_price">
-                        </div>
-                        <div class="form-group">
-                            <label for="title" class="control-label">title</label>
-                            <input type="text" class="form-control " id="title" name="title">
-                        </div>
-                        <div class="form-group">
-                            <label for="img" class="control-label">img</label>
-                            <input type="file" class="form-control" id="img" name="img" />
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button id="itemSubmit" type="submit" class="btn btn-primary">Save</button>
-                </div>
-            </div>
-        </div>
     </div>
-
-    <div class="modal fade" id="editItem" role="dialog" style="display:none">
-        <div class="modal-dialog modal-lg">
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Update item</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form id="updateItem" method="#" action="#" enctype="multipart/form-data">
-                        {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-                        {{-- <input type="hidden" name="_method" value="PUT"> --}}
-                        <div class="form-group">
-                            <label for="idescription" class="control-label">Description</label>
-                            <input type="text" class="form-control" id="idescription" name="description">
-                        </div>
-                        <div class="form-group">
-                            <label for="icost" class="control-label">Cost Price</label>
-                            <input type="text" class="form-control " id="icost" name="cost_price">
-                        </div>
-                        <div class="form-group">
-                            <label for="isell" class="control-label">sell price</label>
-                            <input type="text" class="form-control " id="isell" name="sell_price">
-                        </div>
-                        <div class="form-group">
-                            <label for="ititle" class="control-label">title</label>
-                            <input type="text" class="form-control " id="ititle" name="title">
-                        </div>
-                        <div class="form-group">
-                            <label for="iimg" class="control-label">img</label>
-                            <input type="file" class="form-control" id="iimg" name="img" />
-                        </div>
-                </div>
-                </form>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
-                    <button id="btnItem" type="submit" class="btn btn-primary">Update</button>
-                </div>
-            </div>
-        </div>
     </div>
 
 </body>
