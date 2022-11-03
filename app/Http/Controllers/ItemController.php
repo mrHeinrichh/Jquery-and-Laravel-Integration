675c5bc3c823f5217116b21dd75c030a792c14e5
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-11-03 10:55:24 +0800

    Update Item controller http

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Item;
-use Illuminate\Support\Facades\Storage;
-use View;
 use Illuminate\Support\Facades\File;
+use View;
+use App\Models\Item;
+use App\Models\Order;
+use App\Models\Stock;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class ItemController extends Controller
 {
@@ -49,12 +54,21 @@ class ItemController extends Controller
         $item->sell_price = $request->sell_price;
         $item->cost_price = $request->cost_price;
         $item->title = $request->title;
-        $files = $request->file('uploads');
-        $item->imagePath = 'images/' . $files->getClientOriginalName();
-        $item->save();
-        Storage::put('public/images/' . $files->getClientOriginalName(), file_get_contents($files));
 
-        return response()->json(["success" => "item created successfully.", "item" => $item, "status" => 200]);
+        // $fileName = time().$request->file('imagePath')->getClientOriginalName();
+        // $path = $request->file('imagePath')->storeAs('images', $fileName, 'public');
+        // $requestData["imagePath"] = '/storage/'.$path;
+        // $item->imagePath = $requestData["imagePath"];
+
+        $files = $request->file('uploads');
+
+        $item->imagePath = 'images/' . time() . '-' . $files->getClientOriginalName();
+        $item->save();
+
+        $data = array('status' => 'saved');
+        Storage::put('images/' . time() . '-' . $files->getClientOriginalName(), file_get_contents($files));
+
+        return response()->json(["success" => "Item Created Successfully.", "item" => $item, "status" => 200]);
     }
 
     /**
@@ -90,9 +104,28 @@ class ItemController extends Controller
     public function update(Request $request, $id)
     {
         $item = Item::find($id);
+
+        // $item = Item::find($request->item_id);
+        // dd($request->imagePath);
+        // if ($request->hasFile('imagePath')) {
+        // 	$file = $request->file('imagePath');
+        // 	$fileName = time().'-'.$file->getClientOriginalName();
+        // 	$file->storeAs('images/', $fileName);
+        // 	if ($item->avatar) {
+        // 		File::delete("storage/".$item->imagePath);
+        // 	}
+        // } else {
+        // 	$fileName = $request->itemimage;
+        // }
+
         $item = $item->update($request->all());
 
+        // $itemdata = ['description' => $request->description, 'cost_price' => $request->cost_price, 'sell_price' => $request->sell_price, 'title' => $request->title, 'imagePath' => $fileName];
+
+        // $item->update($itemdata);
+
         $item = Item::find($id);
+
         return response()->json($item);
     }
 
@@ -104,10 +137,11 @@ class ItemController extends Controller
      */
     public function destroy($id)
     {
+
         $item = Item::findOrFail($id);
 
-        if (File::exists("storage/images" . $item->imagePath)) {
-            File::delete("storage/images" . $item->imagePath);
+        if (File::exists("storage/" . $item->imagePath)) {
+            File::delete("storage/" . $item->imagePath);
         }
 
         $item->delete();
@@ -115,4 +149,35 @@ class ItemController extends Controller
         $data = array('success' => 'deleted', 'code' => '200');
         return response()->json($data);
     }
+
+    public function postCheckout(Request $request){
+        $items = json_decode($request->getContent(),true);
+        Log::info(print_r($items, true));
+          try {
+            DB::beginTransaction();
+            $order = new Order();
+            $customer =  Customer::find(1);
+              
+            $customer->orders()->save($order);
+            
+            foreach($items as $item) {
+               $id = $item['item_id'];
+               $order->items()->attach($order->orderinfo_id,['quantity'=> $item['quantity'],'item_id'=>$id]);
+               $stock = Stock::find($id);
+               $stock->quantity = $stock->quantity - $item['quantity'];
+               $stock->save();
+            }
+            
+          }
+        catch (\Exception $e) {
+              DB::rollback();
+              return response()->json(array('status' => 'Order failed','code'=>409,'error'=>$e->getMessage()));
+        }
+      
+          DB::commit();
+          return response()->json(array('status' => 'Order Success','code'=>200,'order id'=>$order->orderinfo_id));
+      
+        
+    }
+
 }
