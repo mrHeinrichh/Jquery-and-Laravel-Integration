75a60a3de79d6761c654ec3fd3116dc63aa3b656
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-11-03 10:55:41 +0800

    Update customer models function

--- app/Models/customer.php
+++ app/Models/customer.php
@@ -4,29 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class customer extends Model
+class Customer extends Model
 {
     use HasFactory;
+    public $table = 'customer';
+    public $primaryKey = 'customer_id';
+    public $timestamps = false;
+    protected $guarded = ['customer_id'];
+    
 
-    protected $table = "customers";
-
-    // protected $guarded = ["id"];
-
-    protected $primaryKey = "customer_id";
-
-    protected $fillable = [
-        'title', 
-        'fname', 
-        'lname',
-        'addressline',
-        'town',
-        'zipcode',
-        'phone',
-        'creditlimit',
-        'level',
-        'user_id'
+    protected $fillable = ['fname','lname',
+        'title','addressline','town','zipcode',
+        'phone','email','user_id'
     ];
 
-    public $timestamps = false;
+     public function orders(){
+
+        return $this->hasMany('App\Models\Order','customer_id');
+
+    }
 }
