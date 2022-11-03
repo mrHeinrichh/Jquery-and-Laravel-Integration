6f2e3078ca1d693b3e3fdb2d7c8a144fd674e20a
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-11-03 08:22:49 +0800

    Update base blade views layouts

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Item</title>
 </head>
+
 <body>
+
     @yield('body')
     @include('layouts.header')
+
 </body>
+
 </html>
